4260ac22bbd6fea6430e72dcfbb51036ee1f1e6e
ChD Xiao <xiaochd@gmail.com>
2016-03-22 15:28:42 +0800

    增加mod调用入口函数

--- WinQ/WinQ.cpp
+++ WinQ/WinQ.cpp
@@ -6,6 +6,7 @@
 #include "PEInfo\PEInfo_main.h"
 #include "PSInfo\PSInfo_main.h"
 #include "UserInfo\UserInfo_main.h"
+#include "ModInfo\ModInfo_main.h"
 
 int main(int argc,char **argv)
 {
@@ -22,6 +23,10 @@ int main(int argc,char **argv)
 	else if (0 == strcmp("-user", argv[1]))
 	{
 		main_user(argc,argv);
+	}
+	else if(0 == strcmp("-mod",argv[1]))
+	{
+		main_mod(argc, argv);
 	}
 
 	getchar();
