6f5f81c0c77f2a12d9890939826fd2d34de96c48
ChD Xiao <xiaochd@gmail.com>
2016-04-06 14:09:47 +0800

    增加task查询入口点函数

--- WinQ/WinQ.cpp
+++ WinQ/WinQ.cpp
@@ -7,26 +7,31 @@
 #include "PSInfo\PSInfo_main.h"
 #include "UserInfo\UserInfo_main.h"
 #include "ModInfo\ModInfo_main.h"
+#include "TaskInfo\TaskInfo_main.h"
 
-int main(int argc,char **argv)
+int main(int argc, char **argv)
 {
 	printf("Hello world!\n");
 
-	if (0 == strcmp("-pe",argv[1]))
+	if (0 == strcmp("-pe", argv[1]))
 	{
 		main_pe(argc, argv);
 	}
-	else if (0 == strcmp("-ps",argv[1]))
+	else if (0 == strcmp("-ps", argv[1]))
 	{
 		main_ps(argc, argv);
 	}
 	else if (0 == strcmp("-user", argv[1]))
 	{
-		main_user(argc,argv);
+		main_user(argc, argv);
 	}
-	else if(0 == strcmp("-mod",argv[1]))
+	else if (0 == strcmp("-mod", argv[1]))
 	{
 		main_mod(argc, argv);
+	}
+	else if (0 == strcmp("-task", argv[1]))
+	{
+		main_task(argc, argv);
 	}
 
 	getchar();
